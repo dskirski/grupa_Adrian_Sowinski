ba705c5019f655faea459a5bc6db81ad3de11f4e
Łukasz Soleski <luk.soleski@gmail.com>
2018-10-15 11:09:40 +0100

    A new asp.net core and angular project has been added. The DBContext class was created and the first migration was added

---
+++ Source/Core/Context/EbookShopContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Core.DataModels;
+namespace Core.Context
+{
+    public class EbookShopContext : DbContext
+    {
+        
+
+        public EbookShopContext(DbContextOptions<EbookShopContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Ebook> Ebooks { get; set; }
+        public DbSet<AuthorEbooks> AuthorEbooks { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // define composite key for the junction table 
+            modelBuilder.Entity<AuthorEbooks>()
+                .HasKey(ae => new { ae.AuthorId, ae.EbookId });
+
+        }
+
+    }
+}
---
+++ Source/EbookShop/Migrations/20181014212111_Initial.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EbookShop.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ebooks",
+                columns: table => new
+                {
+                    EbookId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(maxLength: 255, nullable: false),
+                    ISBN = table.Column<string>(maxLength: 13, nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ebooks", x => x.EbookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 101, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    FB_Token = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorEbooks",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(nullable: false),
+                    EbookId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorEbooks", x => new { x.AuthorId, x.EbookId });
+                    table.ForeignKey(
+                        name: "FK_AuthorEbooks_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorEbooks_Ebooks_EbookId",
+                        column: x => x.EbookId,
+                        principalTable: "Ebooks",
+                        principalColumn: "EbookId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FilePath",
+                columns: table => new
+                {
+                    FilePathId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FileName = table.Column<string>(maxLength: 255, nullable: false),
+                    Path = table.Column<string>(nullable: true),
+                    FileType = table.Column<int>(nullable: false),
+                    EbookId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilePath", x => x.FilePathId);
+                    table.ForeignKey(
+                        name: "FK_FilePath_Ebooks_EbookId",
+                        column: x => x.EbookId,
+                        principalTable: "Ebooks",
+                        principalColumn: "EbookId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorEbooks_EbookId",
+                table: "AuthorEbooks",
+                column: "EbookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FilePath_EbookId",
+                table: "FilePath",
+                column: "EbookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorEbooks");
+
+            migrationBuilder.DropTable(
+                name: "FilePath");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Ebooks");
+        }
+    }
+}
