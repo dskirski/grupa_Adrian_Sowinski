98b6122a18f3557e3e02687cbbc27e96776d850d
Łukasz Soleski <luk.soleski@gmail.com>
2018-11-04 21:36:00 +0100

    Added AutoMapper. Mapping RegistrationDTO to AppUser and setting email as username.

---
+++ Source/EbookShop.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EbookShop.Models;
+using EbookShop.Services.Dtos;
+namespace EbookShop.Services.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            CreateMap<RegistrationDTO,AppUser>()
+                .ForMember(au=> au.UserName,map => map.MapFrom(vm=>vm.Email));
+           
+        }
+    }
+}
