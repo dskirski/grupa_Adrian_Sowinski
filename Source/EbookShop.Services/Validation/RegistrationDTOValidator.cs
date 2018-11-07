58a86245848fc54193022924334f27ed96b32e40
Łukasz Soleski <luk.soleski@gmail.com>
2018-11-07 06:54:05 +0100

    Fluent validation added.

---
+++ Source/EbookShop.Services/Validation/RegistrationDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EbookShop.Services.Dtos;
+using FluentValidation;
+
+namespace EbookShop.Services.Validation
+{
+    public class RegistrationDTOValidator : AbstractValidator<RegistrationDTO>
+    {
+        public RegistrationDTOValidator()
+        {
+            RuleFor(rdto => rdto.Email).Empty(); 
+            RuleFor(rdto => rdto.Password).Empty();
+            RuleFor(rdto => rdto.FirstName).Empty();
+            RuleFor(rdto => rdto.LastName).Empty();
+        }
+    }
+}
