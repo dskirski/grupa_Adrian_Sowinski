d34bd26b5802dd74a62cfafd564b7aa61328464f
Łukasz Soleski <luk.soleski@gmail.com>
2018-11-14 09:00:14 +0100

    Dashboard controller returning user information.

---
+++ Source/EbookShop.Web/Controllers/api/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using EbookShop.Services;
+
+namespace EbookShop.Web.Controllers.api
+{
+    
+    
+    [Authorize("ApiUser")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public DashboardController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var dto = await _userService.IsValidUserHTTP();
+                return Ok(dto);
+            }catch(InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+   
+    }
+}
