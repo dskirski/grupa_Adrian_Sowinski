b808ef84085ba55b68d846b9fd1fdeb637074c0d
Łukasz Soleski <luk.soleski@gmail.com>
2018-11-14 08:57:31 +0100

    Auth controller responsible for login functionality

---
+++ Source/EbookShop.Web/Controllers/api/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using EbookShop.Services;
+using EbookShop.Services.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EbookShop.Web.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login(CredentialsDTO credentialsDto, [FromServices]IAuthenticationService authenticationService)
+        {
+            try
+            {
+                // Returns JWT if credentials are valid otherwise throws InvalidOperationException
+                var jwt = await authenticationService.Authenticate(credentialsDto);
+                return Ok(jwt);
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+    }
+}
