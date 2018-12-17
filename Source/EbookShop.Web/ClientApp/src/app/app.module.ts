241036f7cf94b3b90f2c5379b71bc9f5e4ddf422
Paulina Rudajko <paular507@gmail.com>
2018-12-17 21:12:32 +0100

    ..

--- Source/EbookShop.Web/ClientApp/src/app/app.module.ts
+++ Source/EbookShop.Web/ClientApp/src/app/app.module.ts
@@ -6,22 +6,17 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { NavMenu1Component } from './nav-menu1/nav-menu1.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { StopkaComponent } from './stopka/stopka.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    NavMenu1Component,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
-    StopkaComponent
+    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
