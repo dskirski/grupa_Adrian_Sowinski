65b05decbbfe97e0877f9cd4b0c880cd2ee67b6d
dskirski <d.skirski@gmail.com>
2019-01-23 22:38:11 +0100

    to chyba powinno tak wyglądać, żeby działało

--- Source/EbookShop.Web/ClientApp/src/app/account/login-form/login-form.component.ts
+++ Source/EbookShop.Web/ClientApp/src/app/account/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Console } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../user.service';
 import { Credentials } from '../../credentials';
